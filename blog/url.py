38bc07e082f4588eb07ee7842fdffd15724f67f1
wenyaowu <a129441442@gmail.com>
2015-04-10 16:34:28 -0700

    Basic setting of templates/ static file/ media

---
+++ blog/url.py
@@ -0,0 +1,11 @@
+__author__ = 'evanwu'
+
+from django.conf.urls import patterns, url
+from blog import views
+
+# create url pattern, must named urlpatterns
+urlpatterns = patterns('',
+                       # url(url pattern, view, name)
+                       # http://DomainName/blog/(urlpattern) ---> Go to view
+                       url('r^$', views.index, name='index')
+)
--- myBlog/settings.py
+++ myBlog/settings.py
@@ -11,6 +11,17 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates') # Create an absolute dynamic path
+TEMPLATE_DIRS = (TEMPLATE_PATH,) # Put in the absolute path of template
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+STATIC_URL = '/static/'
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
+STATIC_DIRS = (STATIC_PATH,)
+
+MEDIA_ROOT = os.path.dirname(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 
 # Quick-start development settings - unsuitable for production
@@ -36,6 +47,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -75,13 +87,3 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
-STATIC_URL = '/static/'
-
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR,  'templates'),
-)
