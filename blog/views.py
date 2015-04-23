43e4977484aa18fc515a144609e611b109eb103f
wenyaowu <a129441442@gmail.com>
2015-04-22 22:23:01 -0700

    Clean unnecessary import and codes

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
 from blog.models import Article, Category, Tag
 
 # Create your views here.
