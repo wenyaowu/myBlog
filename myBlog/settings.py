6fdf618911c47d913ae83655ba38bd23c83f4801
wenyaowu <evanwu0225@gmail.com>
2015-04-29 21:22:03 -0700

    Change database setting to postgresql

--- myBlog/settings.py
+++ myBlog/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'myBlog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'myblog',           # Or path to database file if using sqlite3.
+        'USER': 'evanwu',                  # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '104.131.153.24',        # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
