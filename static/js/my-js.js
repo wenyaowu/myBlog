5f5d9b4e258e0ea41d93603439328451a3ac0eb4
Evan Wu <evanwu@codershack.com>
2015-05-04 17:41:51 -0700

    Change animation setting

--- static/js/my-js.js
+++ static/js/my-js.js
@@ -5,7 +5,7 @@ $(function(){
 
     var $window = $(window);
     var $slideimage = $('#djangoLogo');
-    var slidePoint = $slideimage.offset().top;
+    var slidePoint = $slideimage.offset().top-150;
      $('#test2').text("slidePoint:"+slidePoint);
 
     $(window).on('scroll', function(){
